55d46c55da6c1b80dce1345b30242b587f7aec98
norbin <gatinorbert@gmail.com>
2014-02-23 15:41:12 +0100

    Refactored tzdata, Added php.ini configuration, Refactored ServerName, Setted bind-address

--- Vagrantfile
+++ Vagrantfile
@@ -8,11 +8,12 @@ Vagrant.configure("2") do |config|
 
   config.vm.box = "precise32"
 
-config.vm.provision :shell, :path => "bootstrap.sh"
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
 
-  # Create a forwarded port mapping which allows access to a specific port
-  # within the machine from a port on the host machine. In the example below,
-  # accessing "localhost:8080" will access port 80 on the guest machine.
+  config.vm.provision :shell, :path => "bootstrap.sh"
+
   config.vm.network :forwarded_port, guest: 80, host: 4444
+
+  config.vm.network :forwarded_port, guest: 3306, host: 43306
 
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@
 sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 
 #variables
+area="Europe"
+zone="Budapest"
 projectname='vagrant'
 database='vagrant'
 username='vagrant'
@@ -12,19 +14,25 @@ echo ''
 echo '################################################################'
 echo '#            Install Laravel Development Environment           #'
 echo '#                                                              #'
-echo '# PHP 5.4, Apache2.2.22, Mysql, phpMyAdmin, Git, cURL, Laravel #'
+echo '#   PHP 5.4, Apache2.2.22, Mysql, Git, cURL, Xdebug, Laravel   #'
 echo '#                                                              #'
 echo '################################################################'
 echo ''
 
 echo 'Configuring Timezone'
-ln -fs /usr/share/zoneinfo/Etc/GMT-1 /etc/localtime >/dev/null
-ln -fs /usr/share/zoneinfo/Etc/Europe/Budapest /etc/localtime >/dev/null
-echo \"Europe/Budapest\" | sudo tee /etc/timezone
+#Set time zone and time
+echo "tzdata tzdata/Areas select $area" | debconf-set-selections
+echo "tzdata tzdata/Zones/$area select $zone" | debconf-set-selections
+echo $area/$zone > /etc/timezone
+cp /usr/share/zoneinfo/$area/$zone /etc/localtime
+/usr/sbin/ntpdate pool.ntp.org
 dpkg-reconfigure --frontend noninteractive tzdata
 
-echo 'Update Repositories'
+echo 'Update and Upgrade Repositories this takes a while....'
 apt-get update >/dev/null
+sudo apt-get upgrade -y >/dev/null
+echo '...done'
+
 echo 'Install Vim'
 apt-get install -y vim >/dev/null 2>&1
 echo '...done'
@@ -33,15 +41,26 @@ echo '...done'
 ##########################################
 echo 'Install PHP 5.4'
 apt-get install -y python-software-properties >/dev/null
+echo '...done'
 echo 'Adding PHP 5.4 PPA'
 add-apt-repository ppa:ondrej/php5 >/dev/null 2>&1
 apt-get update >/dev/null
+echo '...done'
 echo 'Install dependencies'
 apt-get install -y php5 >/dev/null
 apt-get install -y php5-mcrypt >/dev/null
 apt-get install -y php5-mysql >/dev/null
 apt-get install -y php5-curl >/dev/null
 apt-get install -y php5-cli >/dev/null
+#Error Reporting
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/cli/php.ini
+#Memory Limit
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/cli/php.ini
+#timezone
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/cli/php.ini
 echo '...done'
 
 ##########################################
@@ -49,12 +68,12 @@ echo '...done'
 ##########################################
 echo 'Install Apache2.2.22'
 apt-get install -y apache2 >/dev/null
-echo 'ServerName localhost' >> /etc/apache2/httpd.conf
-echo 'ServerName localhost' >> /etc/apache2/apache2.conf
+sed -i "s/#ServerRoot.*/ServerName localhost/" /etc/apache2/apache2.conf
 a2enmod rewrite >/dev/null
 echo 'Setting up Apache2 virtual host'
 rm -rf /var/www
 ln -fs /vagrant /var/www
+
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
 	ServerName localhost
@@ -74,7 +93,9 @@ VHOST=$(cat <<EOF
 </VirtualHost>
 EOF
 )
+
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
 a2ensite 000-default >/dev/null
 echo 'Apache2 run as vagrant user'
 sed -i '/export APACHE_RUN_USER=www-data/c\export APACHE_RUN_USER=vagrant' /etc/apache2/envvars
@@ -88,6 +109,8 @@ echo '...done'
 echo 'Install Mysql-Server-5.5'
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y mysql-server-5.5 >/dev/null
+sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/mysql/my.cnf
+service mysql restart
 echo 'Create user and database'
 echo "CREATE DATABASE IF NOT EXISTS $database" | mysql
 echo "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'" | mysql
@@ -103,10 +126,13 @@ apt-get install -y git >/dev/null
 curl -s https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 echo '...done'
+
 cd /var/www
 composer create-project laravel/laravel $projectname --prefer-dist
 chmod -R 777 /var/www/$projectname/app/storage
+
 echo 'Create app/config/database.php'
+
 DATABASE=$(cat <<EOF
 <?php
 	return array(
@@ -127,6 +153,7 @@ DATABASE=$(cat <<EOF
 	);
 EOF
 )
+
 echo "$DATABASE" > /var/www/$projectname/app/config/database.php
 
 echo ''
